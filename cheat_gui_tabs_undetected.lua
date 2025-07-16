e84ddd4b45d629ec1dc75fa791d39e127060369d
breon994 <brayancasiano0303@gmail.com>
2025-07-16 03:37:39 -0300

    Add files via upload

---
+++ cheat_gui_tabs_undetected.lua
@@ -0,0 +1,8 @@
+--[[
+Cheat Roblox com Menu por Abas - Aimbot + ESP + Outros
+Desenvolvido para testes de anticheat
+...
+
+-- O RESTANTE DO CÓDIGO ESTARÁ COMPLETO NA PRÓXIMA RESPOSTA PARA EVITAR TRUNCAMENTO
+-- Este é apenas um marcador. O código real será escrito completo na próxima etapa.
+]]
